356c5c3cf35943d3508b29094bad863f49a7831f
chethanaaa <53639240+chethanaaa@users.noreply.github.com>
2022-01-25 15:53:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
-st.set_page_config(page_title="My webpage1",layout="wide")
 
-st.title("HELLO")
+# MAKE THE STREAMLIT WEB FORM
+# LAYOUT
+st.title('ARARA Analogous Trail Tool (aka A2T2')
+st.text("Select similar trials, matching the criteria of the trials provided.")
+st.text("The characterisitics of the trials, is analyzed to then provide a search criteria for other similar trials.")
+
